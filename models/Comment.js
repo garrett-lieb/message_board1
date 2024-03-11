fb645bf5068201a1df8898e265d6277de94715cc
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-03-11 13:28:58 -0700

    remove repeat navbars from all pages but main.handlebars; update formate with prettier

--- models/Comment.js
+++ models/Comment.js
@@ -1,54 +1,45 @@
-const {Model, DataTypes} = require("sequelize");
-const sequelize = require ("../config/connection");
-class Comment extends Model {};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Comment extends Model {}
 
 Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        comment: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        date_created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "post",
-                key: 'id'
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: 'id'
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "Comment"
-    }
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Comment',
+  },
 );
 module.exports = Comment;
-
-
-
-
-
-
-
-
-
