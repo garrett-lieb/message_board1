75b74689e1b29b7cc2226ce6b27e6273341173d9
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-03-11 14:57:52 -0700

    redo formatting and remove unneeded navbars from category pages

--- public/js/newComment.js
+++ public/js/newComment.js
@@ -1,24 +1,25 @@
 const newcommentHandler = async (event) => {
-    event.preventDefault();
-    const comment = document.querySelector('#reply').value.trim();
-    const blog_id = document.querySelector('input[name="post-id"]').value;
-    console.log(comment);
-    if (comment) {
-        const response = await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({ comment, blog_id }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            // document.location.replace(`/blogs/${blog_id}`);
-            document.location.replace('/');
-        }
-        else {
-            // redirect to login page
-            document.location.replace('/login');
-        }
+  event.preventDefault();
+  const comment = document.querySelector('#reply').value.trim();
+  const blog_id = document.querySelector('input[name="post-id"]').value;
+  console.log(comment);
+  if (comment) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment, blog_id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      // document.location.replace(`/blogs/${blog_id}`);
+      document.location.replace('/');
+    } else {
+      // redirect to login page
+      document.location.replace('/login');
     }
-    console.log('comment:', comment);
+  }
+  console.log('comment:', comment);
 };
 
-document.querySelector(".reply-btn").addEventListener('submit', newcommentHandler);
+document
+  .querySelector('.reply-btn')
+  .addEventListener('submit', newcommentHandler);
